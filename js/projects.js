1fdb79e37d7f1b8486ecce58277cef63eb82b676
stuffysaturn <tanaySreedharan@gmail.com>
2024-03-13 12:15:46 -0500

    intial files for digital portfolio

---
+++ js/projects.js
@@ -0,0 +1,91 @@
+var slideIndex1 = 1
+var slideIndex2 = 1
+var slideIndex3 = 1
+
+showImg(slideIndex1)
+showImg2(slideIndex2)
+showImg3(slideIndex3)
+
+function plusDivs(n) {
+    showImg(slideIndex1 += n);
+  }
+  
+function currentDiv(n) {
+    showImg(slideIndex1 = n);
+}
+  
+function showImg(n){
+    var slideArr = document.getElementsByClassName("mySlides")
+    var dots = document.getElementsByClassName("dot")
+    if(n > slideArr.length){
+        slideIndex1 = 1
+    }
+    if(n < 1){
+        slideIndex1 = slideArr.length
+    }
+    for(i = 0; i < slideArr.length; i++){
+        slideArr[i].style.display = "none"
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" dot-active", " dot")
+    }
+    slideArr[slideIndex1-1].style.display = "block"
+    dots[slideIndex1-1].className += " dot-active"
+}
+
+
+function plusDivs2(n) {
+    showImg2(slideIndex2 += n);
+  }
+  
+function currentDiv2(n) {
+    showImg2(slideIndex2 = n);
+}
+  
+
+function showImg2(n){
+    var slideArr = document.getElementsByClassName("mySlides2")
+    var dots = document.getElementsByClassName("dot2")
+    if(n > slideArr.length){
+        slideIndex2 = 1
+    }
+    if(n < 1){
+        slideIndex2 = slideArr.length
+    }
+    for(i = 0; i < slideArr.length; i++){
+        slideArr[i].style.display = "none"
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" dot-active2", " dot2")
+    }
+    slideArr[slideIndex2-1].style.display = "block"
+    dots[slideIndex2-1].className += " dot-active2"
+}
+
+function plusDivs3(n) {
+    showImg3(slideIndex3 += n);
+  }
+  
+function currentDiv3(n) {
+    showImg3(slideIndex3 = n);
+}
+  
+
+function showImg3(n){
+    var slideArr = document.getElementsByClassName("mySlides3")
+    var dots = document.getElementsByClassName("dot3")
+    if(n > slideArr.length){
+        slideIndex3 = 1
+    }
+    if(n < 1){
+        slideIndex3 = slideArr.length
+    }
+    for(i = 0; i < slideArr.length; i++){
+        slideArr[i].style.display = "none"
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" dot-active3", " dot3")
+    }
+    slideArr[slideIndex3-1].style.display = "block"
+    dots[slideIndex3-1].className += " dot-active3"
+}
---
+++ js/script.js
@@ -0,0 +1,25 @@
+/*const effect = VANTA.HALO({
+    el: "#main",
+    mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  size: 1.50
+})*/
+
+VANTA.BIRDS({
+    el: "#main",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    backgroundColor: 0x727272,
+    color1: 0x6b56b6,
+    color2: 0xd1ff,
+    scaleMobile: 1.00,
+    quantity: 4.00,
+    separation: 80.00
+  })
