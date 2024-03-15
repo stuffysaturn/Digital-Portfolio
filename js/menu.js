69e343ed3cd3176224bdc078cc30a9e15debcc3d
stuffysaturn <tanaySreedharan@gmail.com>
2024-03-15 16:09:43 -0500

    added mobile support for entire digital portfolio

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function showMenu(){
+    document.getElementById('back').className = "menuback"
+  }
+
+  function closeMenu(){
+    document.getElementById('back').className = "menuback hide"
+  }
--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-/*const effect = VANTA.HALO({
-    el: "#main",
-    mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  size: 1.50
-})*/
-
 VANTA.BIRDS({
     el: "#main",
     mouseControls: true,
@@ -19,7 +9,7 @@ VANTA.BIRDS({
     backgroundColor: 0x727272,
     color1: 0x6b56b6,
     color2: 0xd1ff,
-    scaleMobile: 1.00,
+    scaleMobile: 3.00,
     quantity: 4.00,
     separation: 80.00
   })
